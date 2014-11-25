3afb00de64b7b3610481c01dd8b895ab0f8933fb
gustavo <gabellog@gmail.com>
2014-11-25 11:02:31 -0500

    Se corregie el descriptor del ear para que que el war pueda ver el ejb, y se agrega una prueba de integración para acceder un EJB remoto desde una jsp

--- ferreteria-EJB/src/main/java/org/gb/ventas/AdministradorVentas.java
+++ ferreteria-EJB/src/main/java/org/gb/ventas/AdministradorVentas.java
@@ -1,11 +1,17 @@
 package org.gb.ventas;
 
+import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
 /**
  * Created by gustavo on 25/11/14.
  */
 @Stateless
+@Remote
 public class AdministradorVentas {
+
+    public String holaAdminVentas(){
+        return "Hola administrador de ventas";
+    }
 
 }
